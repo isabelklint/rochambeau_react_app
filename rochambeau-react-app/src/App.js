0d3e4a6a295e602d2dc6deca8eb4f9021b4e2277
wasline <sf.wasline@gmail.com>
2022-10-01 14:05:35 -0400

    Added Game.js at App.js

--- rochambeau-react-app/src/App.js
+++ rochambeau-react-app/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import Game from "./Game"
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>ROCHAMBEAU</h1>
-        <button>START GAME</button>
+        <Game defaultGame="new game" />
       </div>
+      <footer>
+        This project was coded by Wasline Saint fleur and Isabel Klint is{" "}
+        <a
+          href="https://github.com/isabelklint/rochambeau_react_app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
