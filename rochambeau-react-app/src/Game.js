ce9c4da7483d9a9f4b682586432e875fd8bb85f9
isabelklint <isabel.klint@gmail.com>
2022-10-01 13:11:44 -0500

    added Game.js and Game.css

---
+++ rochambeau-react-app/src/Game.js
@@ -0,0 +1,12 @@
+// import React, { useState } from "react";
+import React from "react";
+import "./Game.css";
+
+
+export default function Game(props) {
+    return (
+      <div>
+          <button type="button" class="btn btn-primary">START GAME</button>
+        </div>
+    );
+  }
