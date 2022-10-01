ddf2019f32becedc230c24ce1ad389da44b2d93c
isabelklint <isabel.klint@gmail.com>
2022-09-30 20:11:47 -0500

    first commit

---
+++ rochambeau-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>Some game</h2>
+      </div>
+    </div>
+  );
+}
