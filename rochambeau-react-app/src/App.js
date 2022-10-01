5d9ab2510a7d5593c0e93857b718a4754414c293
isabelklint <isabel.klint@gmail.com>
2022-10-01 13:13:43 -0500

    Added Game.js and Game.css

--- rochambeau-react-app/src/App.js
+++ rochambeau-react-app/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import Game from "./Game.js";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>ROCHAMBEAU</h1>
-        <button type="button" class="btn btn-primary">START GAME</button>
+        <Game defaultGame="NEW GAME" />
       </div>
     </div>
   );
